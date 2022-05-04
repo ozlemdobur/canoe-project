d3241729673251a26706ea9d032f43f39d462499
nurullahbayram <nurullahbayram@capgemini.com>
2022-05-04 11:56:52 +0200

    test commit 2

--- src/com/capgemini/Controller/UserEditController.java
+++ src/com/capgemini/Controller/UserEditController.java
@@ -2,7 +2,6 @@ package com.capgemini.Controller;
 
 public class UserEditController {
     public void execute(){
-
         System.out.println("UserEditController");
         System.out.println("test commit");
     }
