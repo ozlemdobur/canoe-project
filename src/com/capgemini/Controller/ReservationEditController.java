abc5593ed165f081a61bb4ff1642a3705d54caa1
Khaled7042 <eneskarabey24@gmail.com>
2022-05-08 19:41:21 +0200

    added checking free reservations to the reservation control

--- src/com/capgemini/Controller/ReservationEditController.java
+++ src/com/capgemini/Controller/ReservationEditController.java
@@ -1,38 +1,54 @@
 package com.capgemini.Controller;
 
+import com.capgemini.Model.Canoe;
 import com.capgemini.Model.Model;
 import com.capgemini.Model.Reservation;
+import com.capgemini.Model.Reservations;
 import com.capgemini.View.ReservationEditMenuView;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import static com.capgemini.Main.GREEN_BOLD;
 import static com.capgemini.Main.TEXT_RESET;
 
-public class ReservationEditController {
+public class ReservationEditController{
+
     Model model = new Model();
     ReservationEditMenuView reservationEditMenuView = new ReservationEditMenuView();
+    ReservationProgressController reservationProgressController = new ReservationProgressController(model);
     private String canoeType;
     private String canoeId;
     private String date;
     private String duration;
     private String chosenKey;
+    private String startTime;
+    private String endTime;
+    private String roomNumber;
     int count=-1;
-    public void execute() throws FileNotFoundException{
+    Reservation editedReservation;
+
+
+    public void execute() throws FileNotFoundException, ParseException {
         reservationEditMenuView.executeView();
         String reservationId = reservationEditMenuView.getInputReservationId().trim().toUpperCase(Locale.ROOT);
-        String roomNumber = reservationEditMenuView.getInputRoomNumber().trim().toUpperCase(Locale.ROOT);
 
         boolean check=false;
         for(Reservation reservation : model.getReservations()){
             count++;
-            if(reservationId.equals(reservation.getReservationId())&& roomNumber.equals(reservation.getRoomNumber())){
+            if(reservationId.equals(reservation.getReservationId())){
+                roomNumber=reservation.getRoomNumber();
                 canoeType=reservation.getCanoeType();
                 canoeId=reservation.getCanoeId();
                 date=reservation.getDate();
                 duration=reservation.getDuration();
-                reservationEditMenuView.selectedReservation(reservationId, roomNumber,canoeType,canoeId,date,duration);
+                startTime=reservation.getStartTime();
+                endTime=reservation.getEndTime();
+                Reservation selectedReservation = new Reservation(reservationId,roomNumber,canoeType,canoeId,date,duration,startTime,endTime);
+                reservationEditMenuView.selectedReservation(selectedReservation);
                 check=true;
                 break;
             }
@@ -40,44 +56,172 @@ public class ReservationEditController {
 
         if(check&&reservationEditMenuView.checkReservation()){
             switch (chosenKey = reservationEditMenuView.askForChange().trim().toUpperCase(Locale.ROOT)) {
-                case "RI" -> write(reservationEditMenuView.getNewReservationId(), roomNumber, canoeType, canoeId, date, duration);
-                case "RN" -> write(reservationId, reservationEditMenuView.getNewRoomNumber(), canoeType, canoeId, date, duration);
-                case "CT" -> write(reservationId, roomNumber, reservationEditMenuView.getNewCanoeType(), canoeId, date, duration);
-                case "CI" -> write(reservationId, roomNumber, canoeType, reservationEditMenuView.getNewCanoeId(), date, duration);
-                case "DT" -> write(reservationId, roomNumber, canoeType, canoeId, reservationEditMenuView.getNewDate(), duration);
-                case "DR" -> write(reservationId, roomNumber, canoeType, canoeId, date, reservationEditMenuView.getNewDuration());
-                default -> System.out.println(GREEN_BOLD + "You entered wrong key." + TEXT_RESET);
+                case "RN":
+                    editedReservation = new Reservation(reservationId,reservationEditMenuView.getNewRoomNumber(),canoeType,canoeId,date,duration,startTime,endTime);
+                if(checkFreeReservation(editedReservation)) {
+                    write(editedReservation);
+                }else {reservationEditMenuView.warningMessage();
+                    reservationEditMenuView.noSaved();
+                }
+                    break;
+                case "CT":
+                    String newCanoeType =reservationEditMenuView.getNewCanoeType();
+                    String newCanoeID = checkAvailableCanoeType(newCanoeType,date,startTime,endTime);
+
+                    if(newCanoeID==null){
+                        reservationEditMenuView.warningMessage();
+                        break;
+                    }
+
+                    editedReservation = new Reservation(reservationId, roomNumber,newCanoeType, newCanoeID, date, duration,startTime,endTime);
+                    if(checkFreeReservation(editedReservation)) {
+                    write(editedReservation);
+                    break;
+                    }else {reservationEditMenuView.warningMessage();
+                    reservationEditMenuView.noSaved();
+                    break;
+                    }
+                case "DT":
+                    editedReservation= new Reservation(reservationId, roomNumber, canoeType, canoeId, reservationEditMenuView.getNewDate(), duration,startTime,endTime);
+                    if(checkFreeReservation(editedReservation)) {
+                        write(editedReservation);
+                        break;
+                    }else {reservationEditMenuView.warningMessage();
+                        reservationEditMenuView.noSaved();
+                        break;
+                    }
+
+                case "DR":
+                    duration=reservationEditMenuView.getNewDuration();
+                    editedReservation= new Reservation(reservationId, roomNumber, canoeType, canoeId, date,duration,startTime,reservationProgressController.endTimeCalculating(startTime,duration));
+                if(checkFreeReservation(editedReservation)) {
+                    write(editedReservation);
+                    break;
+                }else {reservationEditMenuView.warningMessage();
+                    reservationEditMenuView.noSaved();
+                    break;
+                }
+                case "ST":
+                    String newSTime = reservationEditMenuView.getNewStartTime().trim();
+                    editedReservation = new Reservation(reservationId, roomNumber, canoeType, canoeId, date, duration, newSTime, reservationProgressController.endTimeCalculating(newSTime, duration));
+                    if(checkFreeReservation(editedReservation)) {
+                        write(editedReservation);
+                        break;
+                    }else {reservationEditMenuView.warningMessage();
+                        reservationEditMenuView.noSaved();
+                        break;
+                    }
+                default: System.out.println(GREEN_BOLD + "You entered wrong key." + TEXT_RESET);
             }
         }else if (!check){
             System.out.println(GREEN_BOLD + "The reservation is not found please check the information and try again." + TEXT_RESET);
         }
     }
 
-    private void write(String reservationId, String roomNumber, String canoeType, String canoeId, String date, String duration) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter("src/com/capgemini/Model/ReservationDB");
-        model.getReservations().remove(count);
-        writer.print("");
-        for (Reservation reservation : model.getReservations()) {
-            writer.println(reservation.getReservationId() + "," +
-                    reservation.getRoomNumber() + "," +
-                    reservation.getCanoeType() + "," +
-                    reservation.getCanoeId() + "," +
-                    reservation.getDate() + "," +
-                    reservation.getDuration());
+
+
+    private void write(Reservation saveReservation) throws FileNotFoundException {
+        if(reservationEditMenuView.saveReservation()) {
+            PrintWriter writer = new PrintWriter("src/com/capgemini/Model/ReservationDB");
+            model.getReservations().set(count, saveReservation);
+            writer.print("");
+            for (Reservation reservation : model.getReservations()) {
+                writer.println(reservation.getReservationId() + "," +
+                        reservation.getRoomNumber() + "," +
+                        reservation.getCanoeType() + "," +
+                        reservation.getCanoeId() + "," +
+                        reservation.getDate() + "," +
+                        reservation.getDuration() + "," +
+                        reservation.getStartTime() + "," +
+                        reservation.getEndTime());
+            }
+            writer.close();
+            message(chosenKey);
         }
-        writer.println(reservationId+","+roomNumber+","+canoeType+","+canoeId+","+date+","+duration);
-        writer.close();
-        message(chosenKey);
     }
+
 
     public void message(String shortcut){
         switch (shortcut) {
-            case "RI" -> reservationEditMenuView.successfulMessage("Reservation ID");
             case "RN" -> reservationEditMenuView.successfulMessage("Room Number");
             case "CT" -> reservationEditMenuView.successfulMessage("Canoe Type");
-            case "CI" -> reservationEditMenuView.successfulMessage("Canoe ID");
+            case "ST" -> reservationEditMenuView.successfulMessage("Start time");
             case "DT" -> reservationEditMenuView.successfulMessage("Date");
             case "DR" -> reservationEditMenuView.successfulMessage("Duration");
         }
     }
+
+
+    public boolean checkFreeReservation(Reservation cReservation){
+        String startT = cReservation.getStartTime();
+        String endT = cReservation.getEndTime();
+        String canoeID = cReservation.getCanoeId();
+        String date = cReservation.getDate();
+        for (Reservation reservation: model.getReservations()){
+           if(date.equals(reservation.getDate())){
+               if(startT.equals(reservation.getStartTime())){
+                   if(canoeID.equals(reservation.getCanoeId())){
+                       return false;
+                   }
+               }
+           }
+        }
+        return true;
+    }
+
+    public String checkAvailableCanoeType(String newCanoeType, String date, String startTime, String endTime) {
+        List<Canoe> availableCanoes = new ArrayList<>();
+        List<Canoe> selectedCanoesType = new ArrayList<>();
+        for (Canoe canoe : model.getCanoes()) {
+            if (canoe.getCanoeType().equals(newCanoeType.trim().toUpperCase(Locale.ROOT))) {
+                selectedCanoesType.add(canoe);
+            }
+        }
+
+        for (Reservation reservation : model.getReservations()) {
+            if (date.equals(reservation.getDate()) && (startTime.equals(reservation.getStartTime()))) {
+                for (Canoe canoe : selectedCanoesType) {
+                    if (!reservation.getCanoeId().equals(canoe.getCanoeId())) {
+                        availableCanoes.add(canoe);
+                    }
+                }
+            }
+            if (date.equals(reservation.getDate()) && endTime.equals(reservation.getEndTime())) {
+                for (Canoe canoe : selectedCanoesType) {
+                    if (!reservation.getCanoeId().equals(canoe.getCanoeId())) {
+                        availableCanoes.add(canoe);
+                    }
+                }
+            }
+        }
+
+        if (availableCanoes.size() == 0) return null;
+        else return availableCanoes.get(0).getCanoeId();
+    }
+
 }
+
+
+/*
+ for (Reservation reservation : model.getReservations()) {
+            if (date.equals(reservation.getDate()) && (startTime.equals(reservation.getStartTime()))) {
+                for (Canoe canoe : selectedCanoes) {
+                    if (!reservation.getCanoeId().equals(canoe.getCanoeId())) {
+                        availableCanoes.add(canoe);
+                    }
+                }
+            }
+                if (date.equals(reservation.getDate()) && endTime.equals(reservation.getEndTime())) {
+                    for (Canoe canoe : selectedCanoes) {
+                        if (!reservation.getCanoeId().equals(canoe.getCanoeId())) {
+                            availableCanoes.add(canoe);
+                        }
+                    }
+                }
+
+                if (availableCanoes.size() == 0) return null;
+                else return availableCanoes.get(0).getCanoeId();
+            }
+
+        return null;
+ */
--- src/com/capgemini/View/ReservationEditMenuView.java
+++ src/com/capgemini/View/ReservationEditMenuView.java
@@ -1,5 +1,7 @@
 package com.capgemini.View;
 
+import com.capgemini.Model.Reservation;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,68 +15,67 @@ public class ReservationEditMenuView {
         return inputReservationId;
     }
 
-    public String getInputRoomNumber() {
-        return inputRoomNumber;
-    }
 
     public void executeView() {
         System.out.println(GREEN_BOLD + "************************************" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "**********  Reservation Edit MENU  *********" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "************************************" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "********************************************" + TEXT_RESET);
         System.out.println(GREEN_BOLD + "**********  RESERVATION EDIT MENU  *********" + TEXT_RESET);
         System.out.println(GREEN_BOLD + "********************************************" + TEXT_RESET);
         System.out.println(GREEN_BOLD + "Please enter the information to edit a Reservation" + TEXT_RESET);
         System.out.print(GREEN_BOLD + "Reservation ID :");
         inputReservationId = sc.nextLine();
-        System.out.print(GREEN_BOLD + "Room Number : ");
-        inputRoomNumber = sc.nextLine();
     }
 
     public String askForChange() {
         System.out.println(GREEN_BOLD + "Please chose which info you want to change " + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Reservation ID (RI)" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Room Number (RN)" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Canoe Type (CT)" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Canoe ID (CI)" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Date (DT)" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Duration (DR)" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "Room Number [RN]" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "Canoe Type [CT]" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "Date [DT]" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "Duration [DR]" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "Start Time [ST]" + TEXT_RESET);
+
 
 
         return sc.nextLine().trim().toUpperCase(Locale.ROOT);
     }
 
-    public void selectedReservation(String reservationId, String roomNumber, String canoeType, String canoeID, String date, String duration) {
+    public void selectedReservation(Reservation reservation) {
         System.out.println(GREEN_BOLD + "Here is the chosen Reservation" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "Reservation ID :" + TEXT_RESET + reservationId);
-        System.out.println(GREEN_BOLD + "Room Number :" + TEXT_RESET + roomNumber);
-        System.out.println(GREEN_BOLD + "Canoe Type :" + TEXT_RESET + canoeType);
-        System.out.println(GREEN_BOLD + "Canoe ID :" + TEXT_RESET + canoeID);
-        System.out.println(GREEN_BOLD + "Date :" + TEXT_RESET + date);
-        System.out.println(GREEN_BOLD + "Duration :" + TEXT_RESET + duration);
+        System.out.println(GREEN_BOLD + "Reservation ID :" + TEXT_RESET + reservation.getReservationId());
+        System.out.println(GREEN_BOLD + "Room Number :" + TEXT_RESET + reservation.getRoomNumber());
+        System.out.println(GREEN_BOLD + "Canoe Type :" + TEXT_RESET + reservation.getCanoeType());
+        System.out.println(GREEN_BOLD + "Canoe ID :" + TEXT_RESET + reservation.getCanoeId());
+        System.out.println(GREEN_BOLD + "Date :" + TEXT_RESET + reservation.getDate());
+        System.out.println(GREEN_BOLD + "Duration :" + TEXT_RESET + reservation.getDuration());
+        System.out.println(GREEN_BOLD + "Start time :" + TEXT_RESET + reservation.getStartTime());
+        System.out.println(GREEN_BOLD + "End Time :" + TEXT_RESET + reservation.getEndTime());
+
 
     }
 
     public boolean checkReservation() {
-        System.out.println(GREEN_BOLD + "Are you sure if it is correct reservation that you want to edit? Yes(Y)/No(N)" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "Are you sure if it is correct reservation that you want to edit? Yes[Y]/No[N]" + TEXT_RESET);
         String yesorno = sc.nextLine().trim().toUpperCase(Locale.ROOT);
         if (yesorno.equals("Y")) {
             return true;
         }
         return false;
     }
+    public boolean saveReservation() {
+        System.out.println(TEXT_RED + "Do yu want to save the changes? Yes[Y]/No[N]" + TEXT_RESET);
+        String yesorno = sc.nextLine().trim().toUpperCase(Locale.ROOT);
+        if (yesorno.equals("Y")) {
+            return true;
+        }
+        System.out.println(TEXT_RED + "Changes are not saved" + TEXT_RESET);
+        return false;
+    }
 
     public void successfulMessage(String chosenKey) {
         System.out.println(TEXT_RED + "Congratulations " + chosenKey + " is changed successfully!!!!" + TEXT_RESET);
-        System.out.println(TEXT_RED + "You changed the reservation !" + TEXT_RESET);
+        System.out.println(TEXT_RED + "You edited the reservation !" + TEXT_RESET);
 
     }
 
-    public String getNewReservationId() {
-        System.out.println(GREEN_BOLD + "Please enter New Reservation ID" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "New Reservation ID :" + TEXT_RESET);
-        return sc.nextLine().trim().toUpperCase(Locale.ROOT);
-    }
 
     public String getNewRoomNumber() {
         System.out.println(GREEN_BOLD + "Please enter New Room Number" + TEXT_RESET);
@@ -88,11 +89,6 @@ public class ReservationEditMenuView {
         return sc.nextLine().trim().toUpperCase(Locale.ROOT);
     }
 
-    public String getNewCanoeId() {
-        System.out.println(GREEN_BOLD + "Please enter Canoe ID" + TEXT_RESET);
-        System.out.println(GREEN_BOLD + "New Canoe ID :" + TEXT_RESET);
-        return sc.nextLine().trim().toUpperCase(Locale.ROOT);
-    }
 
     public String getNewDate() {
         System.out.println(GREEN_BOLD + "Please enter New Date" + TEXT_RESET);
@@ -104,5 +100,24 @@ public class ReservationEditMenuView {
         System.out.println(GREEN_BOLD + "Please enter New Duration" + TEXT_RESET);
         System.out.println(GREEN_BOLD + "New Duration :" + TEXT_RESET);
         return sc.nextLine().trim().toUpperCase(Locale.ROOT);
+    }
+
+    public String getNewStartTime() {
+        System.out.println(GREEN_BOLD + "Please enter New Start Time" + TEXT_RESET);
+        System.out.println(GREEN_BOLD + "New Start Time [hh:mm]:" + TEXT_RESET);
+        String newSTime = sc.nextLine();
+        if (!newSTime.contains(":")) {
+            System.out.println(TEXT_RED + "You entered wrong time format." + TEXT_RESET);
+            return null;
+        }
+        return newSTime;
+    }
+
+    public void warningMessage(){
+        System.out.println(TEXT_RED + "This reservation is already booked, Please select others canoe type, reservation time, date, duration!" + TEXT_RESET);
+    }
+
+    public void noSaved(){
+        System.out.println(TEXT_RED + "Changes are not saved, previous reservation is still booked" + TEXT_RESET);
     }
 }
