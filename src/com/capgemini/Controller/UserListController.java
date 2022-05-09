30a405a5b1487b3b8b5d02a790afc85d9e826e7d
ozlemdobur <104254359+ozlemdobur@users.noreply.github.com>
2022-05-09 23:48:39 +0200

    merge fix

--- src/com/capgemini/Controller/UserListController.java
+++ src/com/capgemini/Controller/UserListController.java
@@ -8,15 +8,25 @@ import java.util.Scanner;
 
 public class UserListController {
     Model model = new Model();
+
     public void execute() {
         model.getUsers().toString();
-        System.out.print(String.format("%-25s","USER NAME"));
-        System.out.print(String.format("%-30s","FULL NAME"));
-        System.out.println(String.format("%-15s","PASSWORD"));
-        for(User user :model.getUsers()){
-            System.out.print(String.format("%-25s",user.getUserId()));
-            System.out.print(String.format("%-30s",user.getUserName()));
-            System.out.println(String.format("%-15s","*******"));
+        System.out.print(String.format("%-25s", "USER NAME"));
+        System.out.print(String.format("%-30s", "FULL NAME"));
+        System.out.print(String.format("%-15s", "PASSWORD"));
+        System.out.println(String.format("%-30s", "ROLE"));
+        for (User user : model.getUsers()) {
+            System.out.print(String.format("%-25s", user.getUserId()));
+            System.out.print(String.format("%-30s", user.getUserName()));
+            System.out.print(String.format("%-15s", "*******"));
+            if (user.getRoleKey().equals("AD")) {
+                System.out.println(String.format("%-30s", "ADMIN"));
+            } else if (user.getRoleKey().equals("GM")) {
+                System.out.println(String.format("%-30s", "MANAGER"));
+            } else if (user.getRoleKey().equals("RE")) {
+                System.out.println(String.format("%-30s", "RECEPTIONIST"));
+            }
+
         }
         Scanner sc = new Scanner(System.in);
         System.out.println("Click enter to continue");
